fd1d79851fa4b454bbb2d1ddd1c6e2ef0ac450c2
praiztech <praisefagbabiaka@gmail.com>
2024-01-08 15:42:41 +0100

    implement task form toggling

--- src/Components/NewTask.js
+++ src/Components/NewTask.js
@@ -1,13 +1,36 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import TaskForm from "./TaskForm";
 
 export default function NewTask({setTasks}) {
+  const [showTaskForm, setShowTaskForm] = useState(false);
+  const addBtnRef = useRef(null);
+
+  useEffect(() => {
+    function hideTaskFormOnEscape(evt) {
+      if (evt.key === 'Escape') {
+        setShowTaskForm(false);
+        addBtnRef.current.focus();
+      }
+    }
+    document.addEventListener('keydown', hideTaskFormOnEscape);
+    return () => document.removeEventListener('keydown', hideTaskFormOnEscape);
+  }, []);
+
+  function hideTaskFormOnFocusOut(evt) {
+    if (!evt.currentTarget.contains(evt.relatedTarget) && showTaskForm) setShowTaskForm(false);
+  }
+
   return (
-    <>
-      <button type="button" aria-expanded="true">
+    <div onBlur={hideTaskFormOnFocusOut}>
+      <button 
+        type="button" 
+        aria-expanded={showTaskForm}
+        onClick={() => setShowTaskForm((prevTaskFormVisibility) => !prevTaskFormVisibility)}
+        ref={addBtnRef}
+      >
         Add New Task
       </button>
-      <TaskForm setTasks={setTasks} />
-    </>
+      {showTaskForm && <TaskForm setTasks={setTasks} />}
+    </div>
   );
 }
