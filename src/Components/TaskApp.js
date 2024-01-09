d673e28e6083f68d36350fff7f5ff15e539eeab2
praiztech <praisefagbabiaka@gmail.com>
2024-01-09 21:25:28 +0100

    implement task list persistence

--- src/Components/TaskApp.js
+++ src/Components/TaskApp.js
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import AddTask from "./AddTask";
 import ShowTasks from "./ShowTasks";
 
 export default function TaskApp() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    return savedTasks === null ? [] : JSON.parse(savedTasks);
+  });
+  const appMounted = useRef(false);
+
+  useEffect(() => {
+    if (!appMounted) { // first render
+      appMounted = true;
+    } else {
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  }, [tasks]);
+  
   return (
     <>
       <AddTask setTasks={setTasks} />
