51a0b57ce6d48aa8cc7ead57dd22a2562bd6bcf1
praiztech <praisefagbabiaka@gmail.com>
2024-01-15 10:36:40 +0100

    refactor task app to use styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import TaskApp from "./components/TaskApp";
 
 export default function App() {
   return (
-    <div className="content">
-      <main>
-        <TaskApp />
-      </main>
-      <footer>Designed & Built by Praise Agbabiaka</footer>
+    <div className="content-bg">
+      <div className="content">
+        <main>
+          <TaskApp />
+        </main>
+        <footer>Designed & Built by Praise Agbabiaka</footer>
+      </div>
     </div>
   );
 }
