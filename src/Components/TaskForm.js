af0950bf2657ffb19b836572ede2cb29441c02c9
praiztech <praisefagbabiaka@gmail.com>
2024-01-17 13:27:44 +0100

    add styles for error and status messages and refactor task form to styles

--- src/Components/TaskForm.js
+++ src/Components/TaskForm.js
@@ -4,9 +4,11 @@ import { useTasksSetter } from "../context/TasksProvider";
 
 export default function TaskForm() {
   const setTasks = useTasksSetter();
-  const [submittedTask, setSubmittedTask] = useState('false');
-  const [newTaskName, setNewTaskName] = useState('');
-  const statusRef = useRef(null);
+  const [newTaskName, setNewTaskName] = useState(''); // sync input data with state - controlled component
+  
+  // submittedName reqd coz after task is added, newTaskName === '' so can't be used in status rendering
+  const [taskData, setTaskData] = useState({submitted: 'false', submittedName: null});
+
   const inputRef = useRef(null);
   const taskId = useRef(null); // self-incrementing counter 4 setting unique ids on tasks
 
@@ -15,38 +17,34 @@ export default function TaskForm() {
   }, []);
 
   function updateTaskName(value) {
-    if (submittedTask === 'new') {
-      // clear status msg when user starts to type after submitting a task
-      statusRef.current.textContent = '';
-    }
-    if (submittedTask !== 'false') setSubmittedTask('false'); // typing a new task, not submiting task
+    // typing a new task, not submiting task
+    if (taskData.submitted !== 'false') setTaskData({submitted: 'false', submittedName: null});
     setNewTaskName(value);
   }
 
   function addNewTask(evt) {
     evt.preventDefault();
     if (newTaskName === '') {
-      setSubmittedTask('blank');
+      setTaskData({submitted: 'blank', submittedName: null});
       inputRef.current.focus();
     } else {
       const newTaskId = ++taskId.current;
       localStorage.setItem('taskId', newTaskId);
-      setSubmittedTask('new');
+      setTaskData({submitted: 'new', submittedName: newTaskName});
       setTasks((prevTasks) => {
         return [
           {id: newTaskId, name: newTaskName, done: false},
           ...prevTasks
         ]
       });
-      statusRef.current.textContent = `Success: ${newTaskName} added.`;
       setNewTaskName(''); // clear input field after task is added
     }
   }
 
   return (
     <>
-      {
-        submittedTask === 'blank' ? (
+      { // live region must always be in DOM to ensure consistent relay of status msg 
+        taskData.submitted === 'blank' ? (
           <form onSubmit={addNewTask}>
             <div className="task-form-box">
               <div className="task-form-entry-box">
@@ -67,31 +65,56 @@ export default function TaskForm() {
                 <AddIcon />
               </button>
             </div>
-            <p id="error-text">Error: Task name cannot be blank. Enter a task to add.</p>
-            <div role="status" ref={statusRef}></div>
+            <div role="status"></div>
+            <p className="error-text" id="error-text">
+              <span className="prefix">Error: </span>Task name cannot be blank. Enter a task to add.
+            </p>
           </form>
-        ) : (
-          <form onSubmit={addNewTask}>
-            <div className="task-form-box">
-              <div className="task-form-entry-box">
-                <input 
-                  type="text" 
-                  id="new-task-name"
-                  autoComplete="off" 
-                  value={newTaskName}
-                  onChange={(evt) => updateTaskName(evt.target.value)} 
-                  ref={inputRef}
-                />
-                <label htmlFor="new-task-name">Enter a task</label>
+        ) : taskData.submitted === 'new' ? (
+            <form onSubmit={addNewTask}>
+              <div className="task-form-box">
+                <div className="task-form-entry-box">
+                  <input 
+                    type="text" 
+                    id="new-task-name"
+                    autoComplete="off" 
+                    value={newTaskName}
+                    onChange={(evt) => updateTaskName(evt.target.value)} 
+                    ref={inputRef}
+                  />
+                  <label htmlFor="new-task-name">Enter a task</label>
+                </div>
+                <button type="submit">
+                  <span className="visually-hidden">Add</span>
+                  <AddIcon />
+                </button>
               </div>
-              <button type="submit">
-                <span className="visually-hidden">Add</span>
-                <AddIcon />
-              </button>
-            </div>
-            <div role="status" ref={statusRef}></div>
-          </form>
-        )
+              <div role="status">
+                <span className="prefix">Success: </span>"{taskData.submittedName}" added. 
+              </div>
+            </form>
+          ) : (
+            <form onSubmit={addNewTask}>
+              <div className="task-form-box">
+                <div className="task-form-entry-box">
+                  <input 
+                    type="text" 
+                    id="new-task-name"
+                    autoComplete="off" 
+                    value={newTaskName}
+                    onChange={(evt) => updateTaskName(evt.target.value)} 
+                    ref={inputRef}
+                  />
+                  <label htmlFor="new-task-name">Enter a task</label>
+                </div>
+                <button type="submit">
+                  <span className="visually-hidden">Add</span>
+                  <AddIcon />
+                </button>
+              </div>
+              <div role="status"></div>
+            </form>
+          )
       }
     </>
   );
