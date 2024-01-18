2bff4bb63ece2e0605bb63d177a1539da9b2aed5
praiztech <praisefagbabiaka@gmail.com>
2024-01-18 17:02:08 +0100

    add task delete confirmation modal and refactor task list to use it

---
+++ src/Components/TaskDeleteModal.js
@@ -0,0 +1,33 @@
+import { useRef, useEffect } from "react";
+
+export default function TaskDeleteModal({ taskName, onCancel, onDelete }) {
+  const deleteModalRef = useRef(null);
+
+  useEffect(() => {
+    deleteModalRef.current.showModal();
+  }, []);
+
+  return (
+    <dialog className="task-delete-modal" ref={deleteModalRef}>
+      <p>Are you sure you want to delete "{taskName}" task?</p>
+      <div className="modal-actions" style={{display:"flex", justifyContent:"space-evenly"}}>
+        <button 
+          onClick={() => {
+            deleteModalRef.current.close();
+            onCancel();
+          }}
+        >
+          Cancel
+        </button>
+        <button 
+          onClick={() => {
+            deleteModalRef.current.close();
+            onDelete();
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </dialog>
+  );
+}
--- src/Components/TaskLists.js
+++ src/Components/TaskLists.js
@@ -1,12 +1,27 @@
 import { DeleteIcon } from "../icons/Icons";
+import { useRef, useState } from "react";
 import { useTasks, useTasksSetter, useAddBtnRef } from "../context/TasksProvider";
-import { useRef } from "react";
+import TaskDeleteModal from "./TaskDeleteModal";
 
 export default function TaskList() {
   const tasks = useTasks();
   const setTasks = useTasksSetter();
   const addBtnRef = useAddBtnRef();
-  const task2FocusOnDeleteIdRef = useRef(null);
+  const task2DeleteIdRef = useRef(null);
+  const task2FocusOnModalCloseRef = useRef(null);
+  const [showTaskDeleteModal, setShowTaskDeleteModal] = useState(false);
+
+  function handleDeleteModalDisplay(task2DeleteId) {
+    task2DeleteIdRef.current = task2DeleteId;
+    setShowTaskDeleteModal(true);
+  }
+
+  function handleTaskDeleteCancel(task2DeleteId) {
+    // set focus back to the triggering ctrl - delete btn, if delete is canceled
+    task2FocusOnModalCloseRef.current = task2DeleteId;
+    task2DeleteIdRef.current = null; // reset - delete handled
+    setShowTaskDeleteModal(false);
+  } 
 
   function handleTaskDelete(task2DeleteId, task2DeleteIndex) {
     setTasks(tasks.filter((task) => task.id !== task2DeleteId));
@@ -14,11 +29,13 @@ export default function TaskList() {
       addBtnRef.current?.focus();
     } else {
       const task2FocusOnDeleteIndex = (
-        // if task 2 delete is last on list, focus will be set 2 preceding task otherwise next task
+        // if task 2 delete is last on list, set focus 2 preceding task otherwise next task
         task2DeleteIndex === tasks.length - 1 ? task2DeleteIndex - 1 : task2DeleteIndex + 1
       );
-      task2FocusOnDeleteIdRef.current = tasks[task2FocusOnDeleteIndex].id;
+      task2FocusOnModalCloseRef.current = tasks[task2FocusOnDeleteIndex].id;
     }
+    task2DeleteIdRef.current = null; // reset - delete handled
+    setShowTaskDeleteModal(false);
   }
 
   function toggleTaskDone(taskId, newDoneValue) {
@@ -40,11 +57,11 @@ export default function TaskList() {
                 checked={task.done}
                 onChange={(evt) => toggleTaskDone(task.id, evt.target.checked)}
                 ref={
-                  task.id === task2FocusOnDeleteIdRef.current ?
+                  task.id === task2FocusOnModalCloseRef.current ?
                   // inline ref callback ensures it's invoked on every render
                   (node) => {
                     if (node !== null) {
-                      task2FocusOnDeleteIdRef.current = null; // reset
+                      task2FocusOnModalCloseRef.current = null; // reset
                       node.focus();
                     }
                   } : undefined
@@ -54,14 +71,34 @@ export default function TaskList() {
               <span className="label-wrapper">
                 <label htmlFor={`${task.id}-checkbox`} id={`${task.id}-name`}>{task.name}</label>
               </span>
-              <button 
-                type="button" 
-                aria-labelledby={`${task.id}-delete ${task.id}-name`}
-                onClick={() => handleTaskDelete(task.id, index)}
-              >
-                <span id={`${task.id}-delete`} hidden>Delete</span>
-                <DeleteIcon />
-              </button>
+              <div className="delete-task">
+                <button 
+                  type="button" 
+                  aria-labelledby={`${task.id}-delete ${task.id}-name`}
+                  onClick={() => handleDeleteModalDisplay(task.id)}
+                  ref={
+                    task.id === task2FocusOnModalCloseRef.current ?
+                      // inline ref callback ensures it's invoked on every render
+                      (node) => {
+                        if (node !== null) {
+                          task2FocusOnModalCloseRef.current = null; // reset
+                          node.focus();
+                        }
+                      } : undefined
+                  }
+                >
+                  <span id={`${task.id}-delete`} hidden>Delete</span>
+                  <DeleteIcon />
+                </button>
+                {
+                  showTaskDeleteModal && task2DeleteIdRef.current === task.id &&
+                  <TaskDeleteModal 
+                    taskName={task.name}  
+                    onCancel={() => handleTaskDeleteCancel(task.id)} 
+                    onDelete={() => handleTaskDelete(task.id, index)}
+                  />
+                }
+              </div>
             </li>
           )
         })
